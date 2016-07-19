7d1d4008f2afdce8f07a79a29c3e2fcfe2638206
kento_hayasaka <thunders.1028@gmail.com>
2016-07-20 05:00:50 +0900

    modify: シンボリックリンクをつくる必要がないファイルをつくらないように変更

--- dotFilesLink.sh
+++ dotFilesLink.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+not_create=(".git" ".gitignore"  ".mac" ".loader" ".common")
 for file in `find ~/dotfiles -name '.*' | awk -F/ '{print $NF}'` ; do
-    if [ ${file} = ".git" ] ; then 
-        echo "skip .git" 
-        continue
-    fi
+    for a_not_create in ${not_create[@]}; do
+      if [ ${file} = ${a_not_create} ]; then
+        echo skip ${a_not_create}
+        continue 2
+      fi
+    done
 
     echo "create symbolic link of" $file
     ln -sf ~/dotfiles/$file ~/$file
@@ -12,5 +15,5 @@ done
 
 chmod 705 ~/dotfiles/.*
 
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+[ ! -d ~/.oh-my-zsh ] && git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-completions ] && git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
